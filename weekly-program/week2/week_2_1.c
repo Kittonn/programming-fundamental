ba58ddaefaa386e623ed7d10a27ed6241b0129ae
Kittipod Lambangchang <tonkittipod@gmail.com>
2022-08-08 23:18:27 +0700

    add : weekly-program
    week 2

---
+++ weekly-program/week2/week_2_1.c
@@ -0,0 +1,11 @@
+// 65010077 - นายกิตติพศ หลำบางช้าง
+
+#include <stdio.h>
+
+int main()
+{
+  int a,b;
+  scanf("%d %d", &a, &b);
+  printf("%d\n%d\n%d\n%d", a + b, a - b, a * b, a / b);
+  return 0;
+}
---
+++ weekly-program/week2/week_2_2.c
@@ -0,0 +1,11 @@
+// 65010077 - นายกิตติพศ หลำบางช้าง
+
+#include <stdio.h>
+
+int main()
+{
+  int n[2];
+  scanf("%d %d", &n[0], &n[1]);
+  printf("%d\n%d\n%d\n%d", n[0] + n[1], n[0] - n[1], n[0] * n[1], n[0] / n[1]);
+  return 0;
+}
